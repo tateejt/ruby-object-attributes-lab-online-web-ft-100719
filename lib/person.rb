93e6a394cc886bb02cfa56337e2e2b0a236b7eda
Emery Tate <tateejt@gmail.com>
2019-10-17 00:20:28 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(the_name)
+    @name = the_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def job=(work_place)
+    @job = work_place
+  end
+  
+  def job
+      @job
+  end 
+  
+end
