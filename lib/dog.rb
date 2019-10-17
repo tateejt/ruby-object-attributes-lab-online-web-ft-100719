f6f653cb1ab1138298932591cac97d36d15db259
Emery Tate <tateejt@gmail.com>
2019-10-17 00:18:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog 
   def name=(this_dogs_name)
-    @dog_name = this_dogs_name
+    @name = this_dogs_name
   end
   
   def name
-    @dog_name
+    @name
   end
   
   def breed=(the_breed)
-    @who_are_you = the_breed
+    @breed = the_breed
   end
   
   def breed 
-    @who_are_you
+    @breed
   end
 end 
